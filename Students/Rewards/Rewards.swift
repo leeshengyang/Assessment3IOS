fbbdb2908c0c6ceac6b601dce9db1a9672958e06
leeshengyang <99948301+leeshengyang@users.noreply.github.com>
2023-05-13 23:00:55 +1000

    Add files via upload
    rewards

---
+++ Students/Rewards/Rewards.swift
@@ -0,0 +1,25 @@
+//
+//  Rewards.swift
+//  Food Donator
+//
+//  Created by UTS on 22/6/2022.
+//
+
+import SwiftUI
+
+struct Rewards: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+//                Image("image1").resizable()
+              //  Image("coupon1").resizable()
+            }
+        }
+    }
+}
+
+struct Rewards_Previews: PreviewProvider {
+    static var previews: some View {
+        Rewards()
+    }
+}
